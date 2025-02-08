514883f2b2a899213927ae8f7c325e5b01959b9b
Junswat <tocotoco2674@gmail.com>
2025-02-08 14:20:18 +0900

    Initial commit with deployment setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/TextConverter/',  // リポジトリ名を指定
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
